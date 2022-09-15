8308b03046f300e24e53ebdf834d5b850831d24e
husnimbark <husnibarking@gmail.com>
2022-09-15 16:50:58 -0700

    updatenow

--- main.go
+++ main.go
@@ -13,13 +13,14 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// handler main
 func main() {
+	// env
 	errEnv := godotenv.Load()
 	if errEnv != nil {
 		panic("Failed to load env file")
 	}
-	// initial database
+
+	// initial DB
 	mysql.DatabaseInit()
 
 	// run migration
@@ -29,14 +30,16 @@ func main() {
 
 	routes.RouteInit(r.PathPrefix("/api/v1").Subrouter())
 
+	//path file
 	r.PathPrefix("/uploads").Handler(http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
+
 	var AllowedHeaders = handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
 	var AllowedMethods = handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS", "PATCH", "DELETE"})
 	var AllowedOrigins = handlers.AllowedOrigins([]string{"*"})
 
+	// Modify 1 line this below code get port from env ...
 	var port = os.Getenv("PORT")
-	fmt.Println("server running localhost:" + port)
 
-	// Embed the setup allowed in 2 parameter on this below code ...
-	http.ListenAndServe("localhost:"+port, handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
+	fmt.Println("server running localhost:" + port)
+	http.ListenAndServe(":"+port, handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
 }
